c1bedee0d21ff5b99986c485815b8ffe182a3773
prockenschaub <rockenschaub.patrick@gmail.com>
2022-08-19 09:38:57 +0200

    Add MMD for time series

--- clinicaldg/algorithms/__init__.py
+++ clinicaldg/algorithms/__init__.py
@@ -3,6 +3,7 @@ from .erm import ERM, ERMID, ERMMerged
 from .irm import IRM
 from .groupdro import GroupDRO
 from .mldg import MLDG
+from .mmd import MMD, CORAL
 from .ann import DANN, CDANN
 from .vrex import VREx
 from .sd import SD

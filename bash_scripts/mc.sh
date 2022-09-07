fd454494b9d33d94a159128750c606d94fa73f89
prockenschaub <rockenschaub.patrick@gmail.com>
2022-09-07 16:16:51 +0200

    Adapt bash to recent changes

--- bash_scripts/mc.sh
+++ bash_scripts/mc.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
-#SBATCH --job-name=cdg-mc
 #SBATCH --output=logs/%x-%A-%a.log
 #SBATCH --ntasks=2
 #SBATCH --mem=50G
 #SBATCH --partition=medium
-#SBATCH --time=15:00:00
+#SBATCH --time=24:00:00
 
 cd ~/work/ClinicalDG # NOTE: Change if your repo lives elsewhere
 
 eval "$($(which conda) shell.bash hook)"
-conda activate clinicaldg
+conda activate clinicaldg-new
 
 set -x
+
+if [ -z ${DS} ]
+then 
+  export DS=mimic
+fi
 
 while IFS="," read -r es algo ts hs seed
 do
     date 
 
     python -m clinicaldg.scripts.train \
-        --experiment MultiCenterMIMIC \
+        --experiment MultiCenter \
         --algorithm ${algo} \
         --es_method ${es} \
-        --hparams '{"mc_architecture": "tcn"}' \
+        --hparams "{\"mc_target_env\": \"${DS}\", \"mc_architecture\": \"tcn\"}" \
         --hparams_seed ${hs} \
         --trial_seed ${ts} \
         --seed ${seed} \
-        --output_dir "outputs/mc/mimic/run${SLURM_ARRAY_TASK_ID}" \
+        --output_dir "outputs/mc/${DS}/run${SLURM_ARRAY_TASK_ID}" \
         --delete_model
+
+    date
 done < <(sed -n "$((${SLURM_ARRAY_TASK_ID}+1))p" "sweeps/mc_params.csv")

067d89123ce4cfee3aa71db2ee59d8c7e2dbad73
prockenschaub <rockenschaub.patrick@gmail.com>
2022-10-17 20:57:24 +0200

    Adapt sweeps

--- sweeps/mc.py
+++ sweeps/mc.py
@@ -4,29 +4,30 @@ from itertools import product
 from clinicaldg.lib import misc
 
 # Define values to sweep over --------------------------------------------------
-oracles = ['ERMID', 'ERMMerged']
-algorithms = ['ERM', 'CORAL', 'VREx', 'IGA']
+algorithms = ['ERM', 'CORAL', 'VREx', 'Fishr', 'GroupDRO', 'MLDG']
 n_trials = 5
 n_hparams = 10
-envs = ['mimic', 'eicu', 'hirid', 'aumc']
-trial_seed = np.arange(n_trials)
+envs = ['miiv', 'eicu', 'hirid', 'aumc']
+trials = np.arange(n_trials)
 hparams_seed = np.arange(n_hparams)
 
 # Generate grid ----------------------------------------------------------------
 
-col_names = ['algorithm', 'test_env', 'val_env', 'trial_seed', 'hparams_seed']
-oracle_grid = product(oracles, envs, ['train'], trial_seed, hparams_seed)
-dg_grid = product(algorithms, envs, ['train'] + envs, trial_seed, hparams_seed)
+col_names = ['algorithm', 'test_env', 'val_env', 'trial', 'hparams_seed']
+merge_grid = product(['ERMMerged'], ['all'], ['train'], trials, hparams_seed)
+id_grid = product(['ERMID'], envs, ['train'], trials, hparams_seed)
+dg_grid = product(algorithms, envs, ['train'] + envs, trials, hparams_seed)
 
 grid = pd.concat((
-    pd.DataFrame(oracle_grid, columns=col_names),
+    pd.DataFrame(merge_grid, columns=col_names),
+    pd.DataFrame(id_grid, columns=col_names),
     pd.DataFrame(dg_grid, columns=col_names)
 ))
 
 seeds = []
 for i in range(grid.shape[0]):
     r = grid.iloc[i, :]
-    s = misc.seed_hash("MultiCenter", r.algorithm, r.hparams_seed, r.trial_seed)
+    s = misc.seed_hash("MultiCenter", r.algorithm, r.hparams_seed, r.trial)
     seeds.append(s)
 grid['seed'] = seeds
 

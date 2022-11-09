368c213051138fb5ffa1162860ffb41271aab71d
prockenschaub <rockenschaub.patrick@gmail.com>
2022-11-09 11:49:02 +0100

    Remove unused algorithms

--- clinicaldg/algorithms/__init__.py
+++ clinicaldg/algorithms/__init__.py
@@ -1,12 +1,7 @@
 
 from .erm import ERM, ERMID, ERMMerged
-from .irm import IRM
 from .groupdro import GroupDRO
 from .mldg import MLDG
 from .mmd import MMD, CORAL
-from .ann import DANN, CDANN
 from .vrex import VREx
-from .sd import SD
-from .rvp import RVP
-from .iga import IGA
 from .fishr import Fishr

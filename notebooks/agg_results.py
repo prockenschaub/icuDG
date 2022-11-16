fb7de8d65e4512033a362175631dc5b10055c283
prockenschaub <rockenschaub.patrick@gmail.com>
2022-11-16 14:43:58 +0100

    Update experiment results

---
+++ notebooks/agg_results.py
@@ -0,0 +1,142 @@
+import plotnine as ggp
+
+from utils import *
+
+
+task = 'icu-mortality'
+path = Path(f'/Users/patrick/clinicaldg-outputs/{task}')
+
+# ------------------------------------------------------------------------------
+# Load and aggregate the results
+grps = ['algorithm', 'val_env', 'test_env']
+res = load_model_performance(path)
+agg_res = aggregate_results(res, grps)
+bst_res = pick_best_result(agg_res, grps)
+summ = summ_mean_std(bst_res, "auroc")
+
+# ------------------------------------------------------------------------------
+# Tabulate ERM performance
+tbl = summ.melt(id_vars=grps+['hparams_seed'])
+
+tbl['train_env'] = tbl['test_env']
+tbl['test_env'] = tbl['variable'].str.extract('^([^_]*)(?=_)')
+
+tbl = tbl[tbl['algorithm'].str.contains("ERM")]
+tbl = tbl[tbl['variable'].str.contains("auroc")]
+tbl = tbl[(tbl['algorithm'] != 'ERM') | (tbl.train_env == tbl.test_env)]
+
+tbl.loc[tbl['algorithm'] == 'ERM', 'train_env'] = 'pooled (n-1)'
+
+erm = tbl.pivot_table(
+    values='value', 
+    index='train_env', 
+    columns = 'test_env', 
+    aggfunc = lambda x: x
+)
+erm.loc[['aumc', 'hirid', 'eicu', 'miiv', 'pooled (n-1)', 'all'], ['aumc', 'hirid', 'eicu', 'miiv']]
+
+
+# ------------------------------------------------------------------------------
+# Plot ERM performance
+plt = tbl.copy()
+plt['value_str'] = plt['value'].str.slice(stop=5)
+plt['value'] = plt['value_str'].astype(float)
+
+g = (ggp.ggplot(plt, ggp.aes('test_env', 'train_env'))
+     + ggp.geom_tile(ggp.aes(fill='value'), colour='white', size=1)
+     + ggp.geom_text(ggp.aes(label='value_str'), colour='white') 
+     + ggp.geom_hline(yintercept=2.5, size=4, colour='white')
+     + ggp.coord_equal() 
+     + ggp.scale_x_discrete(
+        limits=['aumc', 'hirid', 'eicu', 'miiv'],
+        labels=['AUMC', 'HiRID', 'eICU', 'MIMIC']
+       )
+     + ggp.scale_y_discrete(
+        limits=['all', 'pooled (n-1)', 'miiv', 'eicu', 'hirid', 'aumc'],
+        labels=['all', 'pooled (n-1)', 'MIMIC', 'eICU', 'HiRID', 'AUMC']
+       )
+     + ggp.scale_fill_cmap(cmap_name='Blues', limits=[0.5, 1.0], expand=(0, 0))
+     + ggp.guides(fill=ggp.guide_colourbar(barheight=50))
+     + ggp.labs(
+        x='\nEvaluation dataset',
+        y='Training dataset',
+        fill=''
+       )
+     + ggp.theme_minimal()
+     + ggp.theme(
+        axis_text_x=ggp.element_text(size=12),
+        axis_text_y=ggp.element_text(size=12, angle=45), 
+        legend_text=ggp.element_text(size=12),
+        panel_grid=ggp.element_blank()
+       )
+    )
+g
+ggp.ggsave(g, f'figures/erm_{task}.png')
+
+
+# ------------------------------------------------------------------------------
+# Tabulate DG performance
+tbl = summ.melt(id_vars=grps+['hparams_seed'])
+
+tbl['evaluated_in'] = tbl['variable'].str.extract('^([^_]*)(?=_)')
+
+tbl = tbl[~tbl['algorithm'].isin(["ERMMerged", "ERMID"])]
+tbl = tbl[tbl['test_env'] == tbl['evaluated_in']]
+
+dg = tbl.pivot_table(
+    values='value', 
+    index='algorithm', 
+    columns = 'evaluated_in', 
+    aggfunc = lambda x: x
+)
+dg.loc[['ERM', 'CORAL', 'VREx', 'Fishr', 'MLDG', 'GroupDRO'], ['aumc', 'hirid', 'eicu', 'miiv']]
+
+
+# ------------------------------------------------------------------------------
+# Plot DG training process
+
+hparams = load_hparams(path)    
+
+db = 'miiv'
+algo = 'GroupDRO'
+
+# TODO: allow for LOO
+exp = ['algorithm', 'test_env', 'hparams_seed']
+setting = tbl.loc[(tbl['algorithm'] == algo) & (tbl['test_env'] == db), exp]
+runs = setting.merge(res[exp+['trial', 'folder', 'es_step']], on=exp).merge(hparams, on=exp)
+
+progress = load_training_progress(path, db, runs.folder)
+
+if algo == "CORAL":
+    plot_fun = plot_coral
+elif algo == "VREx":
+    plot_fun = plot_vrex
+elif algo == "Fishr":
+    plot_fun = plot_fishr
+elif algo == "MLDG":
+    plot_fun = plot_mldg
+elif algo == "GroupDRO":
+    plot_fun = plot_groupdro
+
+
+plot_fun(runs.merge(progress, on='folder'))
+
+
+# ------------------------------------------------------------------------------
+# Plot DG hyperparameters
+
+id_vars = ['algorithm', 'val_env', 'test_env', 'hparams_seed']
+dg_params = hparams[~hparams['algorithm'].isin(["ERMMerged", "ERMID", "ERM"])].copy()
+dg_params['chosen'] = False
+dg_params.set_index(id_vars, inplace=True)
+bst_idx = dg_params.index.intersection(bst_res.set_index(id_vars).index)
+dg_params.loc[bst_idx, 'chosen'] = True
+dg_params.reset_index(inplace=True)
+
+
+plot_hparam(dg_params[dg_params['algorithm'] == "CORAL"], "mmd_gamma", [-1, 1])
+plot_hparam(dg_params[dg_params['algorithm'] == "VREx"], "vrex_lambda", [-1, 5])
+plot_hparam(dg_params[dg_params['algorithm'] == "Fishr"], "fishr_lambda", [1, 4])
+plot_hparam(dg_params[dg_params['algorithm'] == "MLDG"], "mldg_beta", [-1, 1])
+plot_hparam(dg_params[dg_params['algorithm'] == "GroupDRO"], "groupdro_eta", [-3, -1])
+
---
+++ notebooks/utils.py
@@ -0,0 +1,183 @@
+from pathlib import Path
+import re
+import numpy as np
+import pandas as pd
+import torch
+from tqdm import tqdm
+from typing import List, Dict
+import plotnine as ggp
+
+METHODS = ['ERMMerged', 'ERMID', 'ERM',  'CORAL', 'VREx', 'Fishr', 'GroupDRO', 'MLDG']
+
+def load_single_set_of_stats(path: Path, subset: List = None) -> Dict:
+    stats = torch.load((path/'stats.pkl').open('rb')) 
+    stats['folder'] = path.stem
+    
+    if subset is None:
+        return stats
+    
+    res = {}
+    for elem in subset:
+        x = stats
+        for k in elem.split("/"):
+            x = x[k]
+        if isinstance(x, dict):
+            res.update(x)
+        else:
+            res[k] = x
+    return res
+
+def load_all_stats(path: Path, subset: List = None) -> pd.DataFrame:
+    lst = []
+    for i in tqdm(path.glob('**/done')):
+        stats = load_single_set_of_stats(i.parent, subset)
+        lst.append(stats)
+    return pd.DataFrame(lst)
+
+def load_model_performance(path: Path) -> List:
+    """Load all 
+
+    Args:
+        path: _description_
+
+    Returns:
+        _description_
+    """
+    subset = [
+        "args/algorithm",
+        "model_hparams/val_env",
+        "model_hparams/test_env",
+        "args/hparams_seed",
+        "args/trial",
+        "folder",
+        "es_step",
+        "es_val_loss",
+        "test_results"
+    ]
+    
+    df = load_all_stats(path, subset)
+    df['algorithm'] = pd.Categorical(df['algorithm'], categories=METHODS)
+    df = df.sort_values(['algorithm', 'test_env', 'hparams_seed', 'trial'], ignore_index=True)
+    return df
+
+def load_hparams(path: Path) -> pd.DataFrame:
+    subset = [
+        "args/algorithm",
+        "model_hparams/test_env",
+        "args/hparams_seed",
+        "model_hparams"
+    ]
+    df = load_all_stats(path, subset)
+    df['algorithm'] = pd.Categorical(df['algorithm'], categories=METHODS)
+    df = df.sort_values(['algorithm', 'test_env', 'hparams_seed'], ignore_index=True)
+    return df.drop_duplicates()
+    
+def aggregate_results(df, by):
+    return df.\
+        drop(columns=['trial', 'folder', 'es_step']).\
+        groupby(by+['hparams_seed'], observed=True).\
+        agg(['mean', 'std']).\
+        reset_index()
+
+def pick_best_result(df, by):
+    return df.\
+        groupby(by, observed=True, as_index=False).\
+        apply(lambda x: x.loc[x['es_val_loss']['mean'].idxmin(), :])
+
+def summ_mean_std(df, keep=None):
+    df = df.copy()
+    meta = [c for c, s in df.columns if s == ""]
+    cols = [c for c, s in df.columns if s != ""]
+    _, idx = np.unique(cols, return_index=True)
+    cols = [cols[i] for i in sorted(idx)]
+    df.columns = ["_".join(p) if p[1] != '' else p[0] for p in df.columns]
+    if keep:
+        cols = [c for c in cols if re.search(keep, c)]
+    for c in cols:
+        df[c] = df[f'{c}_mean'].apply('{:.3f}'.format) + u"\u00B1" + df[f'{c}_std'].apply('{:.3f}'.format)
+    return df[meta+cols]
+
+
+def load_training_progress(path, db, folders):
+    progress = []
+    for folder in folders:
+        file = Path(path, db, folder, "results.jsonl")
+        df = pd.read_json(file, lines=True)
+        df['folder'] = folder
+        progress.append(df)
+    return pd.concat(progress, axis=0)
+
+
+def plot_base(progress: pd.DataFrame) -> ggp.ggplot:
+    return (
+        ggp.ggplot(progress, ggp.aes('step', group='trial'))
+            + ggp.geom_line(ggp.aes(y='loss', color=['loss']), show_legend=True) 
+            + ggp.geom_line(ggp.aes(y='val_loss', color=['val']), show_legend=True)
+            + ggp.geom_vline(ggp.aes(xintercept='es_step'), colour='black', show_legend=True)
+            + ggp.scale_color_discrete(name='', limits=['loss', 'val', 'nll', 'penalty'])
+            + ggp.facet_wrap("~ trial")
+            + ggp.labs(x='Training step', y='Loss')
+            + ggp.theme_bw()
+    )
+
+def plot_coral(progress: pd.DataFrame) -> ggp.ggplot:
+    progress['penalty_scld'] = progress['penalty'] * progress['mmd_gamma']
+    return (
+        plot_base(progress)
+        #    + ggp.geom_line(ggp.aes(y='nll', color=['nll']), show_legend=True) TODO: add back in when refactored code is run
+            + ggp.geom_line(ggp.aes(y='penalty', color=['penalty']), linetype='dashed')
+            + ggp.geom_line(ggp.aes(y='penalty_scld', color=['penalty']))
+            
+            + ggp.labs(title='CORAL training progress')
+    )
+
+def plot_vrex(progress: pd.DataFrame) -> ggp.ggplot:
+    progress['penalty_scld'] = progress['penalty'] * progress['vrex_lambda']
+    return (
+        plot_base(progress)
+            + ggp.geom_line(ggp.aes(y='nll', color=['nll']), show_legend=True) 
+            + ggp.geom_line(ggp.aes(y='penalty', color=['penalty']), linetype='dashed')
+            + ggp.geom_line(ggp.aes(y='penalty_scld', color=['penalty']))
+            + ggp.geom_vline(ggp.aes(xintercept='vrex_penalty_anneal_iters'), colour='black', linetype='dashed', show_legend=True)
+            + ggp.labs(title='VREx training progress')
+    )
+
+def plot_fishr(progress: pd.DataFrame) -> ggp.ggplot:
+    progress['penalty_scld'] = progress['penalty'] * progress['fishr_lambda']
+    return (
+        plot_base(progress)
+            + ggp.geom_line(ggp.aes(y='nll', color=['nll']), show_legend=True) 
+            + ggp.geom_line(ggp.aes(y='penalty', color=['penalty']), linetype='dashed')
+            + ggp.geom_line(ggp.aes(y='penalty_scld', color=['penalty']))
+            + ggp.geom_vline(ggp.aes(xintercept='fishr_penalty_anneal_iters'), colour='black', linetype='dashed', show_legend=True)
+            + ggp.labs(title='Fishr training progress')
+    )
+
+def plot_mldg(progress: pd.DataFrame) -> ggp.ggplot:
+    return (
+        plot_base(progress)
+            + ggp.labs(title='MLDG training progress')
+    )
+
+def plot_groupdro(progress: pd.DataFrame) -> ggp.ggplot:
+    return (
+        plot_base(progress)
+            + ggp.labs(title='GroupDRO training progress')
+    )
+
+
+def plot_hparam(hparams, param, limits):
+    return (
+        ggp.ggplot(hparams, ggp.aes('test_env', param, colour='chosen', shape='chosen'))
+        + ggp.geom_point(position=ggp.position_jitter(width=0.2), size=5, show_legend=False)
+        + ggp.scale_y_log10()
+        + ggp.scale_x_discrete(
+            limits=['aumc', 'hirid', 'eicu', 'miiv'],
+            labels=['AUMC', 'HiRID', 'eICU', 'MIMIC']
+        )
+        + ggp.scale_colour_discrete(limits=[True, False])
+        + ggp.scale_shape_manual(name=" ", values=["*", "."], limits=[True, False])
+        + ggp.coord_cartesian(ylim=limits)
+        + ggp.labs(x=" ")
+        + ggp.theme_bw()
+    )

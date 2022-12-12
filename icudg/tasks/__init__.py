a002501dc659626e76ddcf0a9576c1c5ae28ba4c
prockenschaub <rockenschaub.patrick@gmail.com>
2022-12-12 15:55:48 +0100

    remove mnist remnant

--- icudg/tasks/__init__.py
+++ icudg/tasks/__init__.py
@@ -3,6 +3,5 @@
 # Based on code by Zhang et al., rearranged and refactored by 
 # Patrick Rockenschaub. 
 
-from .mnist.task import ColoredMNIST
 from .multicenter.task import Mortality24, AKI, Sepsis
 from .physionet.task import PhysioNet2019

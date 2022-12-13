d09dbbdafeff5fd1593475ecd7274afc6d1c1f21
prockenschaub <rockenschaub.patrick@gmail.com>
2022-12-13 15:51:00 +0100

    add sweeps for best mortality w/ gru

---
+++ bash_scripts/icu_mortality_best.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH --output=logs/%x-%A-%a.log
+#SBATCH --ntasks=2
+#SBATCH --mem=25G
+#SBATCH --partition=medium
+#SBATCH --time=24:00:00
+
+cd ~/work/icuDG # NOTE: Change if your repo lives elsewhere
+
+eval "$($(which conda) shell.bash hook)"
+conda activate icudg
+
+set -x
+
+# If no architecture was set, default to TCN
+if [ -z ${NN} ]
+then 
+  export NN=tcn
+fi
+
+
+while IFS="," read -r hs algo t v ts seed
+do
+    date 
+
+    python -m icudg.train \
+        --task Mortality24 \
+        --algorithm ${algo} \
+        --hparams "{\"test_env\": \"${t}\", \"val_env\": \"${v}\", \"architecture\": \"${NN}\"}" \
+        --hparams_seed ${hs} \
+        --trial ${ts} \
+        --seed ${seed} \
+        --es_metric val_nll \
+        --es_patience 10 \
+        --output_dir "outputs/sepsis_best_${NN}/${t}/run${SLURM_ARRAY_TASK_ID}"
+
+    date
+done < <(sed -n "$((${SLURM_ARRAY_TASK_ID}+1))p" "sweeps/sepsis_best.csv")

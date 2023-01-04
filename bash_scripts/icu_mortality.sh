d4ed86868ea65c46ca7f1955d247225a352aa845
prockenschaub <rockenschaub.patrick@gmail.com>
2023-01-04 12:13:25 +0100

    optimise cluster runs of mort

--- bash_scripts/icu_mortality.sh
+++ bash_scripts/icu_mortality.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #SBATCH --output=logs/%x-%A-%a.log
-#SBATCH --ntasks=2
-#SBATCH --mem=15G
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --mem=0
 #SBATCH --partition=medium
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 
 cd ~/work/icuDG # NOTE: Change if your repo lives elsewhere
 
@@ -13,17 +14,29 @@ conda activate icudg
 
 set -x
 
+# If no START was set, default to 1
+if [ -z ${START} ]
+then 
+  export START=1
+fi
+
+
 # If no architecture was set, default to TCN
 if [ -z ${NN} ]
 then 
   export NN=tcn
 fi
 
-while IFS="," read -r hs algo t v ts seed
-do
-    date 
 
-    python -m icudg.train \
+for i in $(seq 50)
+do
+  row=$(( $START + $i - 1 ))
+
+  while IFS="," read -r hs algo t v ts seed
+  do
+      date 
+
+      python -m icudg.train \
         --task Mortality24 \
         --algorithm ${algo} \
         --hparams "{\"test_env\": \"${t}\", \"val_env\": \"${v}\", \"architecture\": \"${NN}\"}" \
@@ -33,7 +46,8 @@ do
         --es_metric val_nll \
         --es_patience 10 \
         --output_dir "outputs/icu-mortality_${NN}/${t}/run${SLURM_ARRAY_TASK_ID}" \
-        --delete_model
+        --delete_model & 
 
-    date
-done < <(sed -n "$((${SLURM_ARRAY_TASK_ID}+1))p" "sweeps/mc_params_train.csv")
+      date
+  done < <(sed -n "$(($row+1))p" "sweeps/mc_params_train.csv")
+done

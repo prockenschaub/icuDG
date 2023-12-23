fafa12a3a5637b0380ff6e35dd2bca8efbcaa750
prockenschaub <rockenschaub.patrick@gmail.com>
2023-12-23 07:48:53 +0100

    calculate metrics for aki and sepsis

--- notebooks/calculate_all_metrics.py
+++ notebooks/calculate_all_metrics.py
@@ -31,7 +31,7 @@ def calculate_metrics(model_dir):
         trn_envs = task_class.ENVIRONMENTS
         tst_envs = task_class.ENVIRONMENTS
     elif train_args['algorithm'] == 'ERMID':
-        trn_envs = model_dir.parent.name
+        trn_envs = [model_dir.parent.name]
         tst_envs = task_class.ENVIRONMENTS
     elif train_args['algorithm'] == 'ERM':
         trn_envs = [env for env in task_class.ENVIRONMENTS if env != model_dir.parent.name]
@@ -75,43 +75,48 @@ def get_logits_and_outcome(task, algorithm, loader, device='cpu'):
     logits, y, mask = predict_on_set(algorithm, loader, device, task.get_mask)
     mask = cat(mask).to(device)
 
-    logits = logits[..., -1]
-    logits = logits.view(-1)[mask.view(-1)].cpu().numpy()
-    y = y.view(-1)[mask.view(-1)].long().cpu().numpy()
+    probs = torch.sigmoid(logits[..., -1])
+    
+    probs = probs.cpu().numpy()
+    y = y.long().cpu().numpy()
+    mask = mask.int().cpu().numpy()
 
-    return logits, y
+    probs_max, y_max = probs, y
+
+    if len(y.shape) == 2:
+        y_max = (y * mask).max(axis=-1)
+        probs_max = (probs * mask).max(axis=-1)
+
+    probs = probs.reshape(-1)[mask.reshape(-1) == 1]
+    y = y.reshape(-1)[mask.reshape(-1) == 1]
+
+    return probs, y, probs_max, y_max
 
 def do_calc(task, algorithm, val_loader, tst_loader, device='cpu'):
-        val_l, val_y = get_logits_and_outcome(task, algorithm, val_loader, device)
-        tst_l, tst_y = get_logits_and_outcome(task, algorithm, tst_loader, device)
+        _, _, val_p_max, val_y_max = get_logits_and_outcome(task, algorithm, val_loader, device)
+        tst_p, tst_y, tst_p_max, tst_y_max = get_logits_and_outcome(task, algorithm, tst_loader, device)
         
-        auroc = roc_auc_score(tst_y, tst_l)
-        auprc = average_precision_score(tst_y, tst_l)
+        auroc = roc_auc_score(tst_y, tst_p)
+        auprc = average_precision_score(tst_y, tst_p)
 
         # Get sensitivity and PPV at the level of ICU admissions
-        if len(val_y.shape) == 2:
-            val_y = val_y.max(axis=-1)
-            val_l = val_l.max(axis=-1)
-            tst_y = tst_y.max(axis=-1)
-            tst_l = tst_l.max(axis=-1)
-
         def calc_ppv_at(sens):
-            _, roc_sens, roc_thresh = roc_curve(val_y, val_l)
+            _, roc_sens, roc_thresh = roc_curve(val_y_max, val_p_max)
             candidates = roc_thresh[roc_sens >= sens]
             if len(candidates) > 0:
                 sens_thresh = max(candidates)
-                return precision_score(tst_y, tst_l >= sens_thresh)
+                return precision_score(tst_y_max, tst_p_max >= sens_thresh)
             else:
                 return np.nan
 
         ppvs = {sens: calc_ppv_at(sens) for sens in np.arange(0.05, 0.95, 0.05)}
 
         def calc_sens_at(ppv):
-            prc_ppv, _, prc_thresh = precision_recall_curve(val_y, val_l)
+            prc_ppv, _, prc_thresh = precision_recall_curve(val_y_max, val_p_max)
             candidates = prc_thresh[prc_ppv[:-1] >= ppv]
             if len(candidates) > 0:
                 ppv_thresh = min(candidates)
-                return recall_score(tst_y, tst_l >= ppv_thresh)
+                return recall_score(tst_y_max, tst_p_max >= ppv_thresh)
             else:
                 return np.nan
 
@@ -127,19 +132,21 @@ def do_calc(task, algorithm, val_loader, tst_loader, device='cpu'):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Domain generalization')
-    parser.add_argument('--input_dir', type=str, default='/Users/patrick/icudg-outputs/icu-mortality_gru_refit-5')
+    parser.add_argument('--input_dir', type=str, default='/Users/patrick/icudg-outputs/aki_gru_refit-5')
     args = parser.parse_known_args()[0]
 
+    input_dir = Path(args.input_dir)
+
     print(f"Calculate metrics for all models in {args.input_dir}\n")
-    dss = os.listdir(args.input_dir)
+    dss = [f for f in input_dir.iterdir() if f.is_dir()]
     dss.sort()
     for ds in dss:
-        print(f"   Dataset {ds}")
-        runs = os.listdir(os.path.join(args.input_dir, ds))
+        print(f"   Dataset {ds.name}")
+        runs = [f for f in ds.iterdir() if f.is_dir()]
         runs.sort()
         for run in runs:
-            print(f"      {run}")
-            calculate_metrics(Path(os.path.join(args.input_dir, ds, run)))
+            print(f"      {run.name}")
+            calculate_metrics(run)
 
     
         
